afdb5e59c7f036a7b6421b37787e221fce2a6dcf
Joshua Kerr <kerrjoshua@gmail.com>
2023-02-10 10:16:09 +0900

    Apply number action added as handleClick for all digits

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useReducer } from 'react';
 import './App.css';
 
 import reducer, { initialState } from '../reducers/index'
-import { addOne } from '../actions/index'
+import { addOne, applyNumber, changeOperation } from '../actions/index'
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
@@ -12,8 +12,8 @@ import CalcButton from './CalcButton';
 function App() {
 
   const [ state, dispatch ] = useReducer(reducer, initialState);
-  const handleClick = () => {
-    dispatch(addOne())
+  const handleClick = (num) => {
+    dispatch(applyNumber(num))
   }
   return (
     <div className="App">
@@ -38,21 +38,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={handleClick}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleClick(1)}/>
+              <CalcButton value={2} onClick={() => handleClick(2)}/>
+              <CalcButton value={3} onClick={() => handleClick(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleClick(4)}/>
+              <CalcButton value={5} onClick={() => handleClick(5)}/>
+              <CalcButton value={6} onClick={() => handleClick(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleClick(7)}/>
+              <CalcButton value={8} onClick={() => handleClick(8)}/>
+              <CalcButton value={9} onClick={() => handleClick(9)}/>
             </div>
 
             <div className="row">
